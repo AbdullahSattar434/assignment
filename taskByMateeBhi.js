da9a8d045bf952cd1d08ff56a302f893793a3f27
AbdullahSattar <abdullah.sattar@galixo.net>
2024-08-20 00:42:15 -0700

    Operators, done!

--- taskByMateeBhi.js
+++ taskByMateeBhi.js
@@ -42,9 +42,9 @@ console.log("a =", a);*/ // a=4
 
 // Assignment Operators
 
-//1) Equals to  e.g. a=b, --> b is the value given to variable 'a' --> RHS will give some value to the variable at LHS in JS.
+// Equals to  e.g. a=b, --> b is the value given to variable 'a' --> RHS will give some value to the variable at LHS in JS.
 
-//2) "+=" operator
+// "+=" operator
 
 //a+=4 = (a=a+4)
 a+=4;
@@ -65,10 +65,63 @@ console.log("a=", a);
 a **=4;
 console.log("a=", a);
 
+//Comparison operator 
+
 a = 5;
 b = 2;
-
+let c = "5";
+//"== Operator"
 console.log("a=", a, "& b=", b);
+console.log("a==b", 5==2);
+console.log("c==a", c==a);
+
+//"!=" Operator.
+console.log("a!=b",5!=2);
+//"===" Operator.
+console.log("a===c", a===c);
+//"!==" Operator,
+console.log("c!==a", c!==a);
+//>, >=, <, <= Operators
+console.log("a>b", 5>2);
+console.log("a>=b", 5>=2);// if any of the two greater then or equal to is true then the result will also be true.
+console.log("a<b", 5<2);
+console.log("a<=b", 5<=2);
+console.log("b<=a", 2<=5);
+
+//LOGICAL OPERATORS
+
+//a) (&&) Logical AND
+let d = 6;
+let e = 3;
+
+//One way
+let cond1 = d > e;
+let cond2 = d == e;
+let cond3 = d != e;
+
+console.log("cond1 && cond2=", cond1 && cond2);
+
+//Short way for the same thing
+
+console.log("cond1 && cond3", d>e && d!=e);
+
+//b) (||) OR Operator
+console.log("cond1 || cond3", d>e || d!=e);
+console.log("cond1 || cond3", d<e || d!=e);
+console.log("cond1 || cond3", d<e || d==e);
+
+// c) (!) Logical NOT --> can be used only to verify one condition at a time, and will result or give true, false and vice versa.
+console.log("!(d!=e)", !(d!=e));
+console.log("!(6!=3)", !(d!=e));
+console.log("!(d==e)", !(d==e));
+
+alert("Operators, done!");
+
+
+
+
+
+
 
 
 
