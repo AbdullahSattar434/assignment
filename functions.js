292d9528597c652debd23a2be483e509a05c07c9
AbdullahSattar <abdullah.sattar@galixo.net>
2024-08-23 06:01:03 -0700

    functions started

---
+++ functions.js
@@ -0,0 +1,13 @@
+//CH # 5(FUNCTIONS)..........
+console.log("````````````````````````````Functions````````````````````````````");
+//FUNCTIONS........................
+//These are the block of code that perform specific tasks and can be invoked, whenever required.
+
+function myFirstFunc() {
+    console.log("'Welcome to the functions in JS'");
+}
+
+myFirstFunc();
+// whenever i want to use this function, i can write this function and whatever this function contain, it could be a code, operation, condition etc. it will be printed.
+
+
